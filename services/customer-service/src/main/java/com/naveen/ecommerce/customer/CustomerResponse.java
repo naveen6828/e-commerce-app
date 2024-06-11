d919914e773adda917a38c856cebbf0780d9f770
naveen6828 <naveenkumar.jakkula98@gmail.com>
2024-06-11 13:28:18 -0400

    Implemented Customer Service

---
+++ services/customer-service/src/main/java/com/naveen/ecommerce/customer/CustomerResponse.java
@@ -0,0 +1,11 @@
+package com.naveen.ecommerce.customer;
+
+public record CustomerResponse(
+        String id,
+        String firstname,
+        String lastname,
+        String email,
+        Address address
+) {
+
+}
